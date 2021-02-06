e0d704ca5b6849bc53b1991e86ec35a99d073651
ots0121 <ots0121@hotmail.com>
2021-02-05 19:53:14 -0500

    submission

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-# Week-1-assignment
+# Tingsong Ou - Week 1 Assignment
 
-Create a choropleth map for every US county using Geo Albers projection, with the column '% Change in Mortality Rate, 1980-2014' in the file data/mort.csv for the category 'cardiovascular diseases'. If you feel ambitious, please go ahead and make multiple maps for different categories.
+Github Page Link: https://tingsong-ou.github.io/Week-1-Assignment/
 
-_It would be preferable, if you use a different color scheme from the one we used in the class._
+1. Data Processing: 
+
+     (1) I first reduced the mort.csv file size with RStudio by filter out the redundant data, only preserved "Location," "FIPS," "Category," and "% Change in Mortality Rate, 1980-2014" columns. The file size reduced from 15M to 5M. 
+
+     (2) The Eric Tech provides a smaller version of US Counties GeoJSON data with far fewer polygon vertices; this compact GeoJSON file helps to reduce the map loading time from tens of seconds to around 1 second. 
+
+     (3) To match both datasets, the IP value is more reliable than the county name, as county names always overlapped while IP is unique. In mort.csv, "FIPS" represent each county's IP value; in the GeoJSON file, the IP value can be retrieved by combining the State Code (d.property.STATR) and (d.property.COUNTY).
+
+2. Interactive:
+
+     (1) There are 21 categories of mortality rate, so I tried to include all the categories into a dropdown menu, enabling the viewer to make selections. 
+
+     (2) By applying the pattern: d3.select("ELEMENT ID").on("change", function(){}), the visualization will read the data of the selected category, generate new extend and color scale, shade and present the map with the updated the colors.
+
+
+
+## Following are the assignment requirements:
+
+Create a choropleth map for every US county using Geo Albers projection, with the column
+'% Change in Mortality Rate, 1980-2014' in the file data/mort.csv
 
 Please feel free to export a smaller csv file using R or whatever tools you find more comfortable, in order to reduce the file size. You are also encouraged to aesthetically improve the page with usage of better fonts and colors.
 
@@ -11,12 +30,10 @@ Please feel free to export a smaller csv file using R or whatever tools you find
 ##### Maps (geo-json files)
 
 https://github.com/johan/world.geo.json
-
 https://eric.clst.org/tech/usgeojson/
 
 _The required geojson file for this assignment is already present in the /data/maps/ folder_
 
 ##### D3 documentation that might be resourceful here
 [Projections](https://github.com/d3/d3-geo/blob/v2.0.0/README.md#projections)
-
 [Color schemes in D3](https://github.com/d3/d3-scale-chromatic/tree/v2.0.0)
--- script.js
+++ script.js
@@ -1,16 +1,92 @@
-const margin = {t: 50, r:50, b: 50, l: 50};
-const size = {w: 800, h: 800};
+const canvasWidth = document.querySelector('.container').clientWidth;
+
+const margin = {t: 50, r:0, b: 50, l: 0};
+const size = {w: canvasWidth, h: 720};
 const svg = d3.select('svg');
 
 svg.attr('width', size.w)
     .attr('height', size.h);
 
-d3.json('data/maps/us-counties.geo.json')
-    .then(function (mapData) {
+//Creating visualization
+Promise.all(
+    [d3.json("data/maps/us-counties-least.json"),
+    d3.csv('data/MortData_reduced.csv')]
+).then(function(d){
 
+    const mapData = d[0];
+    const mortData = d[1];
+
+    //Creating a list of mortality category
+    const mortCategory = [];
+    for(d of d[1]){
+        if(!mortCategory.includes(d.Category)) mortCategory.push(d.Category);
+    }
+
+    //Creating a dropdowm form
+    dropdowm('selectACategory',mortCategory)
+    
+    let selectedCategory = mortCategory[0];
+    let mortFiltered = mortData.filter(d => d.Category == selectedCategory);
+
+    let mapG = svg.append('g').classed('map', true);
+    let countyPaths = drawMap(mapData, mapG, size);
+    colorMap(countyPaths, mortFiltered);
+
+    //Updating visualization
+    d3.select("#selectACategory").on('change', function(){
+        selectedCategory = d3.select(this).property('value');
+        mortFiltered = mortData.filter(d => d.Category == selectedCategory);
+        colorMap(countyPaths, mortFiltered);
+    });
 });
 
 
-function drawMap (mapData, ele, size) {
 
+//Draw Map Function
+function drawMap (mapData, ele, size) {
+    let projection = d3.geoAlbersUsa()
+        .fitSize([size.w, size.h], mapData);
+    
+    let path = d3.geoPath(projection);
+
+    let pathSel = ele.selectAll("path")
+        .data(mapData.features)
+        .enter()
+        .append('path')
+        .attr('id', d => `${d.properties.STATE}${d.properties.COUNTY}`)
+        .attr('d', d => path(d));
+    
+    return pathSel;
+}
+
+//Shade Map Function
+function colorMap(paths, inputData){
+    let extent = d3.extent(inputData, d => +d['% Change in Mortality Rate, 1980-2014']);
+    let colorScale = d3.scaleSequential()
+        .domain([extent[1],extent[0]])
+        .interpolator(d3.interpolateRdYlBu);
+
+    paths.style('fill', function(d){
+        let countyID = `${d.properties.STATE}${d.properties.COUNTY}`;
+        let value = inputData.filter(d => d.FIPS.padStart(5,'0') === countyID);
+        if(value.length > 0){
+            value = value[0];
+            return colorScale(value['% Change in Mortality Rate, 1980-2014']);
+        }else return '#ccc';        
+    });
+}
+
+
+//Dropdown Form Function
+//reference: https://stackoverflow.com/questions/9895082/javascript-populate-drop-down-list-with-array
+function dropdowm(id, array){
+    let selection = document.getElementById(id);
+
+    for (let i = 0; i<array.length; i++){
+        let value = array[i];
+        let element = document.createElement("option");
+        element.textContent = value;
+        element.value = value;
+        selection.appendChild(element);
+    }
 }
